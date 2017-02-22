8011b136568dfe7e2724ba16a0c84dc040a17bf4
Abhijeet Prasad <abhijeet.prasad@rci.rogers.com>
2017-02-22 15:21:23 -0500

    Initial commit

---
+++ exclusive-content-loader.class.php
@@ -0,0 +1,17 @@
+<?php
+
+class Ex_Content_Loader {
+
+	function __construct() {
+		// Main shortcode.
+		add_shortcode( 'hide', array( $this, 'shortcode' ));
+		
+	}
+
+	function shortcode($atts, $content) {
+		//die($content);
+		if(is_user_logged_in()) {
+			return $content;
+		}
+	}
+}
---
+++ exclusive-content.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Hide This
+ *
+ * This plugin provides a shortcode that lets you hide some parts of the content
+ * from your posts and pages.
+ *
+ * @package   Exclusive-Content
+ * @version   1.0.0
+ * @author    Abhijeet Prasad <abhijeet.prasad@rci.rogers.com>
+ * @link      http://github.com/digitalmedia/
+ *
+ * @wordpress-plugin
+ * Plugin Name: Exclusive Content
+ * Plugin URI: http://github.com/digitalmedia
+ * Description: This plugin provides a shortcode that lets you hide some parts of the content from your posts and pages.
+ * Author: Abhijeet Prasad
+ * Author URI: http://github.com/digitalmedia
+ * Version: 1.0.0
+ */
+
+// Load Hide This class
+require( dirname( __FILE__ ) . '/exclusive-content.class.php' );
+// Load Hide This Loader class
+require( dirname( __FILE__ ) . '/exclusive-content-loader.class.php' );
+
+new Ex_Content_Loader;
