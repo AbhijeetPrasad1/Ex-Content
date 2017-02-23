4c617aeed0c500ec9b3846162416e67e535b9d27
Abhijeet Prasad <abhijeet.prasad@rci.rogers.com>
2017-02-23 09:40:16 -0500

    Created basic options template

--- exclusive-content-loader.class.php
+++ exclusive-content-loader.class.php
@@ -5,7 +5,8 @@ class Ex_Content_Loader {
 	function __construct() {
 		// Main shortcode.
 		add_shortcode( 'hide', array( $this, 'shortcode' ));
-		
+
+		//Ex_Content_Loader::setup();
 	}
 
 	function shortcode($atts, $content) {
@@ -13,7 +14,25 @@ class Ex_Content_Loader {
 		if(is_user_logged_in()) {
 			return $content;
 		} else {
-			return '<p>loyalty club!!!</p>';
+			return '<img src="https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg">';
 		}
 	}
+/*
+	public static function setup() {
+		//add_action('admin_init', 'ex_content_menu');
+	}
+
+	function ex_content_menu() {
+		register_settings('Ex_Content Options', 'Ex_Content', 'manage_options', 'my-unique-indentifier', 'Ex_Content_Options');
+	}
+
+	public static function my_plugin_options() {
+		if ( !current_user_can('manage_options')) {
+			wp_die( __('You cannot access this page.'));
+		}
+		echo '<div class="wrap">';
+		echo '<p>Here is where the form would go if I actually had options.</p>';
+		echo '</div>';
+	}
+	*/
 }
